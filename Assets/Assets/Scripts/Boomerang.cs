bd3a826ecd6761e9796cbfa1e17e046e431a0dda
Miguel Martinez <migueljeffhardie@hotmail.com>
2024-10-19 13:27:05 -0600

    boomerang
    correccion

--- Assets/Assets/Scripts/Boomerang.cs
+++ Assets/Assets/Scripts/Boomerang.cs
@@ -4,23 +4,15 @@ using UnityEngine;
 
 public class Boomerang : MonoBehaviour
 {
-
+    public float speed = 5f; 
+    public Transform player;  
     public float returnSpeed = 3f; 
     private Vector3 initialPosition; 
-
-    public float speed = 10f;
-    public float returnTime = 100f;
-    public Transform player;
-    private Vector3 launchDirection;
-    private float returnTimer;
-
-    private bool returning = false;
-    private Rigidbody2D rb;
+    private bool returning = false; 
 
     void Start()
     {
         initialPosition = transform.position;
-        rb = GetComponent<Rigidbody2D>();   
     }
 
     void Update()
@@ -37,20 +29,12 @@ public class Boomerang : MonoBehaviour
 
     void MoveTowardsPlayer()
     {
-
         if (player != null)
-
-        returnTimer += Time.deltaTime;
-         float t = returnTimer / returnTime;
-       //t = Time.deltaTime;
-          transform.position = Vector3.Lerp(transform.position, player.position, t);
-
-        if (t >=  100f)
-
         {
             Vector3 direction = (player.position - transform.position).normalized;
             transform.position += direction * speed * Time.deltaTime;
 
+            
             if (Vector3.Distance(transform.position, player.position) < 0.5f)
             {
                 returning = true;
@@ -62,13 +46,11 @@ public class Boomerang : MonoBehaviour
     {
         Vector3 direction = (initialPosition - transform.position).normalized;
         transform.position += direction * returnSpeed * Time.deltaTime;
-        rb.bodyType = RigidbodyType2D.Kinematic;
 
-
+       
         if (Vector3.Distance(transform.position, initialPosition) < 0.1f)
         {
             Destroy(gameObject);
         }
-
     }
 }
