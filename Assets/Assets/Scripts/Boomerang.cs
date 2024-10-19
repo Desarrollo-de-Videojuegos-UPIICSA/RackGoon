9659aa44d6502db3d7ff401220f06565aef00fb9
KeyBoardLessM <degelhernandezavalos@gmail.com>
2024-10-19 12:58:56 -0600

    new stuff

--- Assets/Assets/Scripts/Boomerang.cs
+++ Assets/Assets/Scripts/Boomerang.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Boomerang : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public float speed = 5f; 
     public Transform player;  
     public float returnSpeed = 3f; 
     private Vector3 initialPosition; 
+=======
+    public float speed = 10f;
+    public float returnTime = 100f;
+    public Transform player;
+    private Vector3 launchDirection;
+>>>>>>> Stashed changes
     private bool returning = false;
     private Rigidbody2D rb;
 
@@ -31,7 +38,16 @@ public class Boomerang : MonoBehaviour
 
     void MoveTowardsPlayer()
     {
+<<<<<<< Updated upstream
         if (player != null)
+=======
+         returnTimer += Time.deltaTime;
+         float t = returnTimer / returnTime;
+       // float t = Time.deltaTime;
+          transform.position = Vector3.Lerp(transform.position, player.position, t);
+
+        if (t >=  100f)
+>>>>>>> Stashed changes
         {
             Vector3 direction = (player.position - transform.position).normalized;
             transform.position += direction * speed * Time.deltaTime;
