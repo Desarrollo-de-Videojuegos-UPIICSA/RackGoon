934b340b1b045d526fd3e661dea276703bfdb3d0
Miguel Martinez <migueljeffhardie@hotmail.com>
2024-10-19 14:12:36 -0600

    Pinguino_movimiento

---
+++ Assets/Assets/Scripts/Pinguino_movimiento.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pinguino_movimiento : MonoBehaviour
+{
+    public float speed = 5f;
+    public float moveDistance = 3f;
+    private Vector3 startPosition;
+    private bool movingRight = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+            if (transform.position.x >= startPosition.x + moveDistance)
+            {
+                movingRight = false;
+            }
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+            if (transform.position.x <= startPosition.x - moveDistance)
+            {
+                movingRight = true;
+            }
+        }
+    }
+}
