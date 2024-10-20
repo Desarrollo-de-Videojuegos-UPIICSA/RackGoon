bb642701efd5299eaf3aea06953b68ffec23df72
RicardoM-021 <ricardoescobar.ga@gmail.com>
2024-10-19 19:15:12 -0600

    update

--- Assets/Assets/Scripts/destructible_platform.cs
+++ Assets/Assets/Scripts/destructible_platform.cs
@@ -12,7 +12,7 @@ public class destructible_platform: MonoBehaviour
     public void OnCollisionEnter2D(Collision2D collision)
     {
 
-        if (collision.gameObject.CompareTag("Bala") )
+        if (collision.GetContact(0).collider.tag==("Bala" ))
         {
 
             HPMuro--;
