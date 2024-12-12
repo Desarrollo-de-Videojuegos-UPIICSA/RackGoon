a072c4b45425c7e5ffe517e9b62480ae42726e16
SrPatto <ladrianrv@hotmail.com>
2024-12-12 11:38:51 -0600

    Animacion del Main Menu
    - Destello blanco en la pantalla despues de un sonido de escopeta
    - Aparesca chispita con los botones y que empice la musica
    ----
    - Arreglo de los assets del fondo, letras, personajeTitulo
    - Agregue la musica del menu principal

--- Assets/Menuinicial.cs
+++ Assets/Menuinicial.cs
@@ -1,17 +1,98 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class Menuinicial : MonoBehaviour
 {
+    public GameObject Title;
+    public GameObject Logo;
+    public Canvas Buttons;
+    public Image FlashPanel;
+    public AudioSource soundEffectSource; // Fuente de audio para efecto de escopeta
+    public AudioSource musicSource; // Fuente de audio para la música
+
+    private bool isAnimating;
+
+    private void Start()
+    {
+        Logo.SetActive(false);
+        Buttons.enabled = false;
+        var color = FlashPanel.color;
+        color.a = 0.0f;
+        FlashPanel.color = color;
+
+        isAnimating = true;
+    }
+
+    public void FixedUpdate()
+    {
+        if (isAnimating)
+        {
+            StartCoroutine(MenuAnimation());
+            isAnimating = false; // Para asegurarnos de que la corutina solo se inicie una vez
+        }
+    }
+
+    IEnumerator MenuAnimation()
+    {
+        // Titulo cayendo
+        yield return new WaitForSeconds(1f);
+        
+        // Disparo de escopeta y fade in
+        yield return StartCoroutine(FadeIn());
+        
+        // Fade out
+        yield return StartCoroutine(FadeOut());
+        
+        FlashPanel.gameObject.SetActive(false);
+    }
+
+    IEnumerator FadeIn()
+    {
+        // Reproducir sonido antes del fading
+        if (!soundEffectSource.isPlaying)
+        {
+            soundEffectSource.Play();
+        }
+
+        while (FlashPanel.color.a < 1)
+        {
+            var color = FlashPanel.color;
+            color.a += Time.fixedDeltaTime;
+            FlashPanel.color = color;
+            yield return null;
+        }
+
+        Logo.SetActive(true);
+        Buttons.enabled = true;
+    }
+
+    IEnumerator FadeOut()
+    {
+        while (FlashPanel.color.a > 0)
+        {
+            var color = FlashPanel.color;
+            color.a -= Time.fixedDeltaTime;
+            FlashPanel.color = color;
+            yield return null;
+        }
+
+        // Reproducir música después del fading
+        if (!musicSource.isPlaying)
+        {
+            musicSource.Play();
+        }
+    }
 
     public void BotonPlay()
     {
-
         // Debug.Log("clicking game time");
         SceneManager.LoadScene(2);
-
     }
 
     public void Botonexit()
@@ -19,5 +100,4 @@ public class Menuinicial : MonoBehaviour
         Debug.Log("Salir");
         Application.Quit();
     }
-
 }
