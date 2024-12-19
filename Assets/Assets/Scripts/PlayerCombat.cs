933ddd660cba4a8280524085d8476c1d9607d4da
AleInformatic <bryan.ab0702@gmail.com>
2024-12-19 00:16:56 -0600

    Sistema de vidas
    Se añadio sistema de vidas en el script de player combat pero falta añadir las imagenes de los corazones en el array de los corazones

--- Assets/Assets/Scripts/PlayerCombat.cs
+++ Assets/Assets/Scripts/PlayerCombat.cs
@@ -6,39 +6,85 @@ public class PlayerCombat : MonoBehaviour
 {
     private PlayerMovement playerMovement;
     private Animator animator;
-    
+
+    public int maxLives = 2; // Máximo número de corazones (vidas)
+    private int currentLives; // Vidas actuales del jugador
+    public GameObject[] hearts; // Array de objetos UI que representan los corazones
+
     public float controlLoseTimer;
-    
-    // Start is called before the first frame update
+
     void Start()
     {
         playerMovement = GetComponent<PlayerMovement>();
         animator = GetComponent<Animator>();
+
+        // Inicializar vidas
+        currentLives = maxLives;
+        UpdateHeartsUI();
     }
 
     public void TakeDamage(float damage)
     {
-        //perder vida
+        // Reducir vida
+        currentLives--;
+        UpdateHeartsUI();
+
+        if (currentLives <= 0)
+        {
+            Die();
+        }
     }
 
     public void TakeDamage(float damage, Vector2 position)
     {
-        //perder vida
-        
-        //animator.SetTrigger("Damage");
-        
-        //Perder control
-        StartCoroutine(LoseControl());
-        StartCoroutine(DisableCollider());
-        playerMovement.Knockback(position);
+        // Reducir vida
+        currentLives--;
+        UpdateHeartsUI();
+
+        if (currentLives <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            // Reaccionar al daño
+            StartCoroutine(LoseControl());
+            StartCoroutine(DisableCollider());
+            playerMovement.Knockback(position);
+        }
     }
-    
+
+    private void UpdateHeartsUI()
+    {
+        // Actualizar la visualización de los corazones
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < currentLives)
+            {
+                hearts[i].SetActive(true); // Mostrar corazón si está dentro del número de vidas actuales
+            }
+            else
+            {
+                hearts[i].SetActive(false); // Ocultar corazón si está fuera de las vidas actuales
+            }
+        }
+    }
+
+    private void Die()
+    {
+        // Acción al morir
+        Debug.Log("El jugador ha muerto.");
+        playerMovement.canMove = false;
+        // Puedes añadir una animación de muerte, reinicio de nivel, etc.
+    }
+
     private IEnumerator DisableCollider()
     {
         Physics2D.IgnoreLayerCollision(7, 8, true);
         yield return new WaitForSeconds(controlLoseTimer);
         Physics2D.IgnoreLayerCollision(7, 8, false);
     }
+
     private IEnumerator LoseControl()
     {
         playerMovement.canMove = false;
