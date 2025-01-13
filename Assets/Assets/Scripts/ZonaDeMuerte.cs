87d4f98d00c3c6cc84abd691221ec02908da2dfb
Miguel Martinez <migueljeffhardie@hotmail.com>
2025-01-12 21:07:24 -0600

    Update ZonaDeMuerte.cs
    se cambio la zona de muerte a escena actual

--- Assets/Assets/Scripts/ZonaDeMuerte.cs
+++ Assets/Assets/Scripts/ZonaDeMuerte.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class ZonaDeMuerte : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-        if (collision.GetContact(0).collider.tag==("Player"))
+        if (collision.GetContact(0).collider.tag == "Player")
         {
+            // Obtiene la escena actual
+            Scene currentScene = SceneManager.GetActiveScene();
 
-            SceneManager.LoadScene("GameScene");
-
+            // Recarga la escena actual
+            SceneManager.LoadScene(currentScene.name);
         }
     }
 }
