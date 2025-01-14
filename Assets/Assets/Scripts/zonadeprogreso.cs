e28c0379bcc3e03ba9cfe3f296ae17bc486f6040
Zucaritas0-0 <gearman2003@gmail.com>
2025-01-13 20:37:31 -0600

    Adición de zona de progreso

---
+++ Assets/Assets/Scripts/zonadeprogreso.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class zonadeprogreso : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.GetContact(0).collider.tag == "Player")
+        {
+            // Obtiene la escena actual
+            Scene currentScene = SceneManager.GetActiveScene();
+
+            // Recarga la escena actual
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
