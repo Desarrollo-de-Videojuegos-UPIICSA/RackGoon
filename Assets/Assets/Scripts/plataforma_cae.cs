85d017f6e3b6bbbe8a3fcb62937ec5891e9949b5
SrPatto <ladrianrv@hotmail.com>
2025-01-13 23:31:04 -0600

    hotfix zoo

--- Assets/Assets/Scripts/plataforma_cae.cs
+++ Assets/Assets/Scripts/plataforma_cae.cs
@@ -6,16 +6,18 @@ public class plataforma_cae : MonoBehaviour
 {
     public float fallDelay = 0.3f;
     public float shakeAmount = 5f;
-    bool readyToShake = false;
-
-    Rigidbody2D rb;
-    Vector3 originalPos;
-    Collider2D coli;
+    public float respawnDelay = 3f;
+    public bool readyToShake = false;
+    
+    public Rigidbody2D rb;
+    public Vector3 originalPos;
+    public Collider2D coli;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         coli = GetComponent<Collider2D>();  
+        originalPos = transform.position;
     }
 
     void FixedUpdate()
@@ -25,7 +27,6 @@ public class plataforma_cae : MonoBehaviour
             Vector3 newPos = originalPos + Random.insideUnitSphere * (Time.deltaTime * shakeAmount);
             newPos.y = transform.position.y;
             newPos.z = transform.position.z;
-
             transform.position = newPos;
         }
     }
@@ -35,21 +36,27 @@ public class plataforma_cae : MonoBehaviour
         if (col.gameObject.CompareTag("Player"))
         {
             StartCoroutine(Falling(fallDelay));
-            //  rb.constraints = RigidbodyConstraints2D.None;
-            
         }
     }
 
     IEnumerator Falling(float delay)
     {
-        originalPos = transform.position;
         yield return new WaitForSeconds(delay);
-     
 
         readyToShake = true;
         yield return new WaitForSeconds(1.0f);
-        rb.bodyType = RigidbodyType2D.Dynamic;
-        coli.enabled = false;
-        
+        rb.bodyType = RigidbodyType2D.Dynamic; 
+        coli.enabled = false; 
+        yield return new WaitForSeconds(respawnDelay); 
+        RespawnPlatform();
+    }
+
+    void RespawnPlatform()
+    {
+        readyToShake = false;
+        rb.bodyType = RigidbodyType2D.Static; 
+        rb.velocity = Vector2.zero; 
+        transform.position = originalPos; 
+        coli.enabled = true; 
     }
 }
